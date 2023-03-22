a77baa8e3060df444a9445642a61dc43a4278632
Erik Rosvall <erirosv@gmail.com>
2023-03-22 23:54:29 +0100

    added some small stuff

--- FiTrans/server.py
+++ FiTrans/server.py
@@ -4,3 +4,15 @@ from threading import Thread
 
 # Global variables
 IP_FOR_TCP = 'localhost'
+PORT_FOR_TCP = 9000
+BUFF_SIZE = 2048
+
+# Class ClientAccess
+class ClientAccess(Thread):
+    def __init__(slef):
+        pass
+
+    def execute(self):
+        pass
+
+# Run the program
