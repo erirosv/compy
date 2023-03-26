fa3f7007f5c604ea98ded20410fcac3a1f497b19
Erik Rosvall <erirosv@gmail.com>
2023-03-26 16:19:51 +0200

    serverside complete

---
+++ FiTrans/README.md
@@ -0,0 +1,13 @@
+# A simple server client connection
+
+This is only using ```localhost``` as an example. Change the setting to an IP-address if you want to use it on the LAN.
+
+This server does send an item from the server to the client. This is only as a pfoor of concept state. It can be muche more. 
+
+## TODO
+Add an simple interface for the server and client (terminal or GUI). 
+
+Add funtionallity:
+- [] Send/recieve
+    - [] on file
+    - [] all files in dir
--- FiTrans/server.py
+++ FiTrans/server.py
@@ -38,4 +38,34 @@ class ClientAccess(Thread):
                 break
         print(f'{self.ip_addr}:{self.port} [+] File Recieved: {self.f_name}')
 
-# Run the program
+#  *** Run the program *** 
+
+# setting for the server
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_socket.bind((IP_FOR_TCP, PORT_FOR_TCP))
+threads = []
+
+# show message
+print('[ + ] Server Status: Ready...')
+while True:
+    tcp_socket.listen(3) # Experiment to get the correct setting
+    
+    # accepted connecntion from client to server
+    (connection ,(ip_addr, port)) = tcp_socket.accept()
+    # printing info to user screen
+    print(f'Successful connection: {ip_addr}:{port}')
+    print(f'File requested: {ip_addr}:{port} ', end='')
+
+    # Sending the files (in byte foramt)
+    connection.send(b'Options: ')
+    request_client_file_name = connection.recv(1024).decode()
+    print(request_client_file_name)
+
+    # create new client on a new thread by calling the class
+    thread = ClientAccess(ip_addr, port, connection, filename=request_client_file_name, path='images/')
+    thread.start()
+    thread.append(thread)
+
+# This part does remove the thread when it has completed its task
+for thread in threads:
+    thread.join()
